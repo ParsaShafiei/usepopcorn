7f810513fbc91184b7eca49ee7f18bba81569466
unknown <parsashafiei1995@gmail.com>
2024-08-14 19:53:14 -1200

    add movie

--- src/index.js
+++ src/index.js
@@ -1,26 +1,16 @@
 import React, { useCallback, useState } from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
+import './index.css';
+import App from './App';
 
 import StarRating from './StarRating';
 
-function Test(){
-  const [movieRating, setMovieRating] = useState(0);
-  return(
-    <div>
-      <StarRating maxRating={10} color='blue' onSetRating={setMovieRating}/>
-      <p>this movie has {movieRating} Stars</p>
-    </div>
-  );
-}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating />
-    <StarRating size='24' color='red' />
-    <Test />
+    <App />
+
   </React.StrictMode>
 );
